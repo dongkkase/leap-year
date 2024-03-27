25ebb722793caff3a3240dc4482bc995ac1a6183
acema-sungbin <sb.jung@acema.co.kr>
2024-03-28 08:11:49 +0900

    php 윤년체크

---
+++ index.php
@@ -0,0 +1,11 @@
+echo date("L", strtotime("2019-01-01")); // 0
+echo date("L", strtotime("2020-01-01")); // 1
+echo date("L", strtotime("2021-01-01")); // 0
+echo date("L", strtotime("2022-01-01")); // 0
+echo date("L", strtotime("2023-01-01")); // 0
+echo date("L", strtotime("2024-01-01")); // 1
+ 
+if (date("L", strtotime("2020-01-01")) == true)
+{
+    echo "윤년입니다.";
+}
